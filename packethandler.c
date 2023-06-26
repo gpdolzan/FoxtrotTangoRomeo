3199d53501b781598f0f70ea6ce4b25de3e67f94
Gabriel Dolzan <gabrieldolzan@icloud.com>
2023-06-26 19:55:47 -0300

    trying to rec

--- packethandler.c
+++ packethandler.c
@@ -233,7 +233,7 @@ int sendFile(int socket, char *filename, int filesize)
                     printf("sequencia nova: %d\n", sequence);
                     break;
                 }
-                else if(serverPacket.tipo == NACK && serverPacket.sequencia == sequence)
+                else if(serverPacket.tipo == NACK && serverPacket.sequencia != sequence)
                 {
                     // Enviar novamente
                     printf("RECEBI NACK, ENVIANDO DADOS DE NOVO!\n");
@@ -353,6 +353,7 @@ int receiveFile(int socket, char* filename, int filesize)
                 // Send NACK
                 printf("Recebi sequencia %d, esperava %d\n", clientPacket.sequencia, expectedSequence);
                 createPacket(&serverPacket, 0, (expectedSequence + 1), NACK, NULL); // Pedido para receber proximo DATA
+                printf("Enviando NACK com sequencia %d\n", (expectedSequence + 1));
                 sendPacket(socket, &serverPacket);
             }
             else if(clientPacket.tipo == FIM_ARQ)
